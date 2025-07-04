0cb8a47a8463f5cd080744d30521253d5f36c904
David Venhoek <david@tweedegolf.com>
2025-07-04 11:00:40 +0200

    Define traits for server management.

---
+++ nts-pool-ke/src/servers.rs
@@ -0,0 +1,57 @@
+use std::{
+    collections::{HashMap, HashSet},
+    net::SocketAddr,
+};
+
+use tokio::io::{AsyncRead, AsyncWrite};
+
+use crate::{
+    error::PoolError,
+    nts::{AlgorithmDescription, AlgorithmId, ProtocolId},
+};
+
+pub trait ServerManager: Sync + Send {
+    type Server<'a>: Server + 'a
+    where
+        Self: 'a;
+
+    /// Select a server for a client at the given address, taking into acount
+    /// any denied servers.
+    ///
+    /// Denied servers need not be respected if no other options are available
+    fn assign_server(&self, address: SocketAddr, denied_servers: &[String]) -> Self::Server<'_>;
+}
+
+pub trait Server: Sync + Send {
+    type Connection<'a>: ServerConnection + 'a
+    where
+        Self: 'a;
+
+    /// Name of the server, to be passed in the server record if the server
+    /// itself doesn't provide one.
+    fn name(&self) -> &str;
+
+    /// Fetch which protocols and algorithms a server supports.
+    fn support(
+        &self,
+    ) -> impl Future<
+        Output = Result<
+            (
+                HashSet<ProtocolId>,
+                HashMap<AlgorithmId, AlgorithmDescription>,
+            ),
+            PoolError,
+        >,
+    > + Send;
+
+    /// Open a connection to the server.
+    fn connect<'a>(
+        &'a self,
+    ) -> impl Future<Output = Result<Self::Connection<'a>, PoolError>> + Send;
+}
+
+pub trait ServerConnection: AsyncRead + AsyncWrite + Unpin + Send {
+    /// Return the connection to be reused later.
+    #[allow(unused)]
+    fn reuse(self) -> impl Future<Output = ()> + Send;
+}
