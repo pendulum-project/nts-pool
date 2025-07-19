15c24111c9cc3086de913157902f77f1016f752b
David Venhoek <david@tweedegolf.com>
2025-07-19 15:53:49 +0200

    Added/reworked testing for the new geolocation based manager.

--- nts-pool-ke/src/servers.rs
+++ nts-pool-ke/src/servers.rs
@@ -100,3 +100,98 @@ async fn fetch_support_data(
         .collect();
     Ok((supported_protocols, supported_algorithms))
 }
+
+#[cfg(test)]
+mod tests {
+    use std::sync::Mutex;
+
+    use super::*;
+
+    struct TestConnection<'a> {
+        written: &'a Mutex<Vec<u8>>,
+        read_data: &'a [u8],
+    }
+
+    impl AsyncRead for TestConnection<'_> {
+        fn poll_read(
+            mut self: std::pin::Pin<&mut Self>,
+            _cx: &mut std::task::Context<'_>,
+            buf: &mut tokio::io::ReadBuf<'_>,
+        ) -> std::task::Poll<std::io::Result<()>> {
+            let to_write = self.read_data.len().min(buf.remaining());
+
+            let (now, fut) = self.read_data.split_at(to_write);
+            self.read_data = fut;
+            buf.put_slice(now);
+
+            std::task::Poll::Ready(Ok(()))
+        }
+    }
+
+    impl AsyncWrite for TestConnection<'_> {
+        fn poll_write(
+            self: std::pin::Pin<&mut Self>,
+            _cx: &mut std::task::Context<'_>,
+            buf: &[u8],
+        ) -> std::task::Poll<Result<usize, std::io::Error>> {
+            let mut written = self.written.lock().unwrap();
+            written.extend_from_slice(buf);
+            std::task::Poll::Ready(Ok(buf.len()))
+        }
+
+        fn poll_flush(
+            self: std::pin::Pin<&mut Self>,
+            _cx: &mut std::task::Context<'_>,
+        ) -> std::task::Poll<Result<(), std::io::Error>> {
+            //noop
+            std::task::Poll::Ready(Ok(()))
+        }
+
+        fn poll_shutdown(
+            self: std::pin::Pin<&mut Self>,
+            _cx: &mut std::task::Context<'_>,
+        ) -> std::task::Poll<Result<(), std::io::Error>> {
+            //noop
+            std::task::Poll::Ready(Ok(()))
+        }
+    }
+
+    impl ServerConnection for TestConnection<'_> {
+        async fn reuse(self) { /* noop */
+        }
+    }
+
+    #[tokio::test]
+    async fn test_query_supporting_servers() {
+        let mut received = Mutex::new(vec![]);
+        let connection = TestConnection {
+            written: &received,
+            read_data: &[
+                0xC0, 4, 0, 6, 0, 0, 0, 1, 0, 2, 0xC0, 1, 0, 8, 0, 0, 0, 16, 0, 1, 0, 32, 0x80, 0,
+                0, 0,
+            ],
+        };
+
+        let (protocols, algorithms) = fetch_support_data(connection, &HashSet::from([0, 1]))
+            .await
+            .unwrap();
+        assert!(protocols.contains(&0));
+        assert!(protocols.contains(&1));
+        assert_eq!(protocols.len(), 2);
+
+        assert_eq!(
+            algorithms.get(&0),
+            Some(&AlgorithmDescription { id: 0, keysize: 16 })
+        );
+        assert_eq!(
+            algorithms.get(&1),
+            Some(&AlgorithmDescription { id: 1, keysize: 32 })
+        );
+
+        let req = ServerInformationResponse::parse(received.get_mut().unwrap().as_slice())
+            .await
+            .unwrap();
+        assert_eq!(req.supported_algorithms.len(), 0);
+        assert_eq!(req.supported_protocols.len(), 0);
+    }
+}
