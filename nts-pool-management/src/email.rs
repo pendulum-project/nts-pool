0678c9d07a5e14b1e73e27fa0f864445439378ee
Ruben Nijveld <ruben@tweedegolf.com>
2025-08-14 09:10:23 +0200

    Setup email template for registration

--- nts-pool-management/src/email.rs
+++ nts-pool-management/src/email.rs
@@ -1,9 +1,10 @@
 use std::str::FromStr;
 
 use anyhow::Context;
+use askama::Template;
 use lettre::{AsyncSmtpTransport, AsyncTransport, Message, Tokio1Executor};
 
-use crate::error::AppError;
+use crate::{error::AppError, templates::AppVars};
 
 pub type MailTransport = AsyncSmtpTransport<Tokio1Executor>;
 
@@ -22,6 +23,13 @@ impl Mailer {
     }
 }
 
+#[derive(Template)]
+#[template(path = "email/activation.txt.j2")]
+struct ActivationTemplate<'a> {
+    user: &'a crate::models::user::User,
+    activation_token: &'a str,
+}
+
 pub(crate) async fn send_activation_email(
     mailer: &Mailer,
     user: &crate::models::user::User,
@@ -30,13 +38,20 @@ pub(crate) async fn send_activation_email(
         return Err(anyhow::anyhow!("User does not have an activation token").into());
     };
 
+    let body_content = ActivationTemplate {
+        user,
+        activation_token,
+    }
+    .render()
+    .context("Failed to render activation email")?;
+
     let message = Message::builder()
         .to(lettre::Address::from_str(&user.email)
             .context("Failed to parse email address")?
             .into())
         .from(mailer.from_address.clone())
         .subject("Activation code")
-        .body(format!("Your activation code is: {}", activation_token))
+        .body(body_content)
         .context("Failed to generate email")?;
     mailer
         .transport
