24b196220e3de38c9027b3a07ebb017216178d69
Ruben Nijveld <ruben@tweedegolf.com>
2025-08-22 21:22:13 +0200

    Make sure nts-pool-ke still tests correctly when all tests are run workspace wide

--- nts-pool-ke/src/lib.rs
+++ nts-pool-ke/src/lib.rs
@@ -126,3 +126,11 @@ async fn run(options: NtsPoolKeOptions) -> Result<(), Box<dyn std::error::Error>
         }
     }
 }
+
+#[cfg(test)]
+#[ctor::ctor]
+fn test_init() {
+    rustls::crypto::aws_lc_rs::default_provider()
+        .install_default()
+        .expect("Failed to install default crypto provider");
+}
