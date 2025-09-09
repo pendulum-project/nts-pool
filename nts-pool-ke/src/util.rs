8815cd7a0c898d097b3c7801e152881f4ab095c5
David Venhoek <david@tweedegolf.com>
2025-09-09 12:04:14 +0200

    [KELB] Dynamically reload tls configuration in the backend.

--- nts-pool-ke/src/util.rs
+++ nts-pool-ke/src/util.rs
@@ -72,3 +72,34 @@ pub fn load_certificates(
 ) -> Result<Vec<rustls::pki_types::CertificateDer<'static>>, rustls::pki_types::pem::Error> {
     rustls::pki_types::CertificateDer::pem_file_iter(path)?.collect()
 }
+
+/// Join handle that automatically aborts the task when dropped.
+///
+/// Can be used to automatically stop update tasks once they are no longer needed.
+pub struct AbortingJoinHandle<T>(tokio::task::JoinHandle<T>);
+
+impl<T> From<tokio::task::JoinHandle<T>> for AbortingJoinHandle<T> {
+    fn from(value: tokio::task::JoinHandle<T>) -> Self {
+        AbortingJoinHandle(value)
+    }
+}
+
+impl<T> std::ops::Deref for AbortingJoinHandle<T> {
+    type Target = tokio::task::JoinHandle<T>;
+
+    fn deref(&self) -> &Self::Target {
+        &self.0
+    }
+}
+
+impl<T> std::ops::DerefMut for AbortingJoinHandle<T> {
+    fn deref_mut(&mut self) -> &mut Self::Target {
+        &mut self.0
+    }
+}
+
+impl<T> Drop for AbortingJoinHandle<T> {
+    fn drop(&mut self) {
+        self.0.abort();
+    }
+}
