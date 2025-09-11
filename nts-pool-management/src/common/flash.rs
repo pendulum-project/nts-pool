83d6003ce51a52a5036a1b2062649258a44b7af7
cikzh <marlonpeeters@tweedegolf.com>
2025-09-11 17:36:07 +0200

    Added type to cookie, need to impl in template

--- nts-pool-management/src/common/flash.rs
+++ nts-pool-management/src/common/flash.rs
@@ -1,18 +1,29 @@
-use std::convert::Infallible;
+use std::{convert::Infallible, fmt::Display};
 
 use axum::{extract::FromRequestParts, http::request::Parts, response::IntoResponseParts};
 use axum_extra::extract::{PrivateCookieJar, cookie::Cookie};
 
 use crate::AppState;
 
-enum MessageType {
+pub const FLASH_COOKIE_NAME: &str = "flash";
+
+pub enum MessageType {
     Success,
     Error,
 }
 
-pub struct FlashMessage(pub PrivateCookieJar);
+impl Display for MessageType {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        match self {
+            MessageType::Success => write!(f, "success"),
+            MessageType::Error => write!(f, "error"),
+        }
+    }
+}
 
-impl FromRequestParts<AppState> for FlashMessage {
+pub struct FlashMessageService(pub PrivateCookieJar);
+
+impl FromRequestParts<AppState> for FlashMessageService {
     type Rejection = Infallible;
 
     async fn from_request_parts(
@@ -24,13 +35,16 @@ impl FromRequestParts<AppState> for FlashMessage {
     }
 }
 
-impl FlashMessage {
-    pub fn set(&self, message: String) -> Self {
-        Self(self.0.clone().add(Cookie::new("flash", message)))
+impl FlashMessageService {
+    pub fn set(&self, level: MessageType, message: String) -> Self {
+        Self(self.0.clone().add(Cookie::new(
+            FLASH_COOKIE_NAME,
+            format!("{}|{}", level, message),
+        )))
     }
 }
 
-impl IntoResponseParts for FlashMessage {
+impl IntoResponseParts for FlashMessageService {
     type Error = Infallible;
 
     fn into_response_parts(
@@ -38,5 +52,25 @@ impl IntoResponseParts for FlashMessage {
         res: axum::response::ResponseParts,
     ) -> Result<axum::response::ResponseParts, Self::Error> {
         self.0.into_response_parts(res)
+    }
+}
+
+pub fn extract_flash_message(cookie_jar: PrivateCookieJar) -> (PrivateCookieJar, Option<String>) {
+    if let Some(flash) = cookie_jar.get(FLASH_COOKIE_NAME) {
+
+        if let Some((msg_type, msg)) = flash.value().split_once("|") {
+            (
+                cookie_jar.remove(FLASH_COOKIE_NAME),
+                Some(msg.to_string()),
+            )
+        } else {
+            (
+                cookie_jar.remove(FLASH_COOKIE_NAME),
+                None,
+            )
+        }
+
+    } else {
+        (cookie_jar, None)
     }
 }
