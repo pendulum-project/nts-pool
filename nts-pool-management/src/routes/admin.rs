f32a091c2d53ac270251bbe6f1b326dfe07332a0
David Venhoek <david@tweedegolf.com>
2025-09-17 14:04:01 +0200

    [Management] Add functionality for admins to manage monitors.

--- nts-pool-management/src/routes/admin.rs
+++ nts-pool-management/src/routes/admin.rs
@@ -1,17 +1,22 @@
 use askama::Template;
 use axum::{
+    Form,
     extract::{Path, State},
     response::{IntoResponse, Redirect},
 };
 use axum_extra::extract::CookieJar;
 use eyre::{Context, OptionExt, eyre};
+use serde::Deserialize;
 
 use crate::{
     AppState,
-    auth::{Administrator, JwtClaims, login_into},
+    auth::{self, Administrator, JwtClaims, login_into},
     context::AppContext,
     error::AppError,
-    models::user::{self, User, UserId},
+    models::{
+        monitor::{self, Monitor, MonitorId, NewMonitor},
+        user::{self, User, UserId},
+    },
     templates::{HtmlTemplate, filters},
 };
 
@@ -42,6 +47,77 @@ pub async fn users(
 ) -> Result<impl IntoResponse, AppError> {
     let users = user::list(&state.db).await?;
     Ok(HtmlTemplate(UsersTemplate { app, users }))
+}
+
+#[derive(Template)]
+#[template(path = "admin/monitors.html.j2")]
+struct MonitorsTemplate {
+    app: AppContext,
+    monitors: Vec<Monitor>,
+}
+
+pub async fn monitors(
+    _admin: Administrator,
+    app: AppContext,
+    State(state): State<AppState>,
+) -> Result<impl IntoResponse, AppError> {
+    let monitors = monitor::list(&state.db).await?;
+    Ok(HtmlTemplate(MonitorsTemplate { app, monitors }))
+}
+
+#[derive(Template)]
+#[template(path = "admin/monitor_key.html.j2")]
+struct MonitorKeyTemplate {
+    app: AppContext,
+    name: String,
+    key: String,
+}
+
+#[derive(Debug, Deserialize, Clone)]
+#[serde(rename_all = "kebab-case")]
+pub struct NewMonitorForm {
+    pub name: String,
+}
+
+pub async fn create_monitor(
+    _admin: Administrator,
+    app: AppContext,
+    State(state): State<AppState>,
+    Form(NewMonitorForm { name }): Form<NewMonitorForm>,
+) -> Result<impl IntoResponse, AppError> {
+    let authentication_key = auth::generate_monitor_token();
+    monitor::create(
+        &state.db,
+        NewMonitor {
+            name: name.clone(),
+            authentication_key: authentication_key.clone(),
+        },
+    )
+    .await?;
+
+    Ok(HtmlTemplate(MonitorKeyTemplate {
+        app,
+        name,
+        key: authentication_key,
+    }))
+}
+
+pub async fn rekey_monitor(
+    _admin: Administrator,
+    app: AppContext,
+    Path(monitor_id): Path<MonitorId>,
+    State(state): State<AppState>,
+) -> Result<impl IntoResponse, AppError> {
+    let authentication_key = auth::generate_monitor_token();
+    let monitor =
+        monitor::update_authentication_key(&state.db, monitor_id, authentication_key.clone())
+            .await?;
+
+    Ok(HtmlTemplate(MonitorKeyTemplate {
+        app,
+        name: monitor.name,
+        key: authentication_key,
+    }))
 }
 
 pub async fn user_block(
