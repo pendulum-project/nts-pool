750ae83891ebbbd9bc6c5e089462627840526ae7
Marlon Baeten <marlon@tweedegolf.com>
2025-10-02 12:28:43 +0200

    Improve javascript livereload polling mechanism

--- nts-pool-management/assets/poll.js
+++ nts-pool-management/assets/poll.js
@@ -1,27 +1,30 @@
 // setup abort controller to clean up fetches when navigating to another page
 const controller = new AbortController();
-let shouldShortPoll = true;
 window.addEventListener("beforeunload", () => {
   controller.abort();
-  shouldShortPoll = false;
 });
 
 // long polling, when the server is up we get a 200 response every 30s
 const longPoll = () => {
-  fetch("/livereload/poll", { cache: "no-store", signal: controller.signal  })
+  fetch("/livereload/poll", {
+    cache: "no-store",
+    signal: controller.signal,
+  })
     .catch(() => {
-      if (shouldShortPoll) {
+      if (!controller.signal.aborted) {
         console.log("[livereload] disconnected");
         shortPoll();
       }
     })
     .then((r) => {
-      if (r.ok) {
-        console.log("[livereload] heartbeat");
-        longPoll();
-      } else if (shouldShortPoll) {
-        console.log("[livereload] disconnected");
-        shortPoll();
+      if (!controller.signal.aborted) {
+        if (r && r.ok) {
+          console.log("[livereload] heartbeat");
+          longPoll();
+        } else {
+          console.log("[livereload] disconnected");
+          shortPoll();
+        }
       }
     });
 };
@@ -33,14 +36,14 @@ const shortPoll = () => {
     signal: AbortSignal.timeout(500),
   })
     .then((r) => {
-      if (!r.ok) {
-        void setTimeout(shortPoll, 500)
-      } else {
+      if (r && r.ok) {
         window.location.reload();
+      } else {
+        void setTimeout(shortPoll, 500);
       }
     })
     .catch(() => {
-      void setTimeout(shortPoll, 500)
+      void setTimeout(shortPoll, 500);
     });
 };
 
